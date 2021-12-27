95243d4e47cc7c96277b534ec5bbcc4e4181fa8c
trackingknm <96720157+trackingknm@users.noreply.github.com>
2021-12-27 16:00:58 +0700

    Create code-tracking.js

---
+++ code-tracking.js
@@ -0,0 +1,42 @@
+var coffeNav = document.querySelectorAll('div.btn-ar > a > .col-auto.pr-0');
+
+if(coffeNav != undefined){
+
+	for (var i = 0; i < coffeNav.length; i++) {
+	   coffeNav[i].addEventListener("click", function() {//console.log("Tester")
+			//console.log("Tester "+this.textContent)
+			var navigationAttr = {};
+			
+			//console.log("Navigation "+JSON.stringify(navigationAttr));
+
+			gtmDataObject.push({
+				event : "customEvent",
+				eventCategory : "User Engagement",
+				eventAction : "Navigation - Welcome to Nespresso",
+				eventLabel: "Let's get started - "+this.textContent
+			});
+	   
+	   });
+	}
+}
+
+var wellNessNav = document.querySelectorAll('.feat-ar > .f-tc > .center-ct > .px-3 >div > .btn-01.med');
+
+if(wellNessNav != undefined){
+	for (var i = 0; i < wellNessNav.length; i++) {
+	   wellNessNav[i].addEventListener("click", function() {console.log("Tester")
+			console.log("Tester "+this.textContent)
+			var navigationAttr = {};
+			
+			//console.log("Navigation "+JSON.stringify(navigationAttr));
+
+			gtmDataObject.push({
+				event = 'customEvent',
+				eventCategory = "User Engagement",
+				eventAction = "Navigation - Welcome to Nespresso",
+				eventLabel= "Welcome to Nespresso - "+this.textContent
+			});
+	   
+	   });
+	}
+}
