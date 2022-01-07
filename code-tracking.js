09947f9101197761b170aafc1e72f0e07a58e74c
trackingknm <96720157+trackingknm@users.noreply.github.com>
2022-01-07 13:05:55 +0700

    Update code-tracking.js

--- code-tracking.js
+++ code-tracking.js
@@ -21,6 +21,27 @@ window.onload = function () {
 
 		}
 	}
+	
+	var wellNessNav = document.querySelectorAll('.feat-ar > .f-tc > .center-ct > .px-3 >div > .btn-01.med');
+
+	if(wellNessNav != undefined){
+		for (var i = 0; i < wellNessNav.length; i++) {
+		   wellNessNav[i].onclick = function () {
+			console.log("Tester "+this.textContent)
+			var navigationAttr = {};
+
+			//console.log("Navigation "+JSON.stringify(navigationAttr));
+
+			gtmDataObject.push({
+				event = 'customEvent',
+				eventCategory = "User Engagement",
+				eventAction = "Navigation - Welcome to Nespresso",
+				eventLabel= "Welcome to Nespresso - "+this.textContent
+			});
+
+		   }
+		}
+	}
 }
 
 /*window.onload=function(){
