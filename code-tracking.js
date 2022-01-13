860561684873eb28ce04bf27dc0fe9a17854e59d
trackingknm <96720157+trackingknm@users.noreply.github.com>
2022-01-13 15:38:29 +0700

    Update Tracking Registration

--- code-tracking.js
+++ code-tracking.js
@@ -44,52 +44,114 @@ window.onload = function () {
 		   }
 		}
 	}
+	
+	//registration tracking
+var registerButton = document.querySelector("button#validate-privilege");
+
+if(typeof(document.querySelector('button#validate-privilege')) != 'undefined' && document.querySelector('button#validate-privilege') != null){
+
+
+    registerButton.onclick = function(){
+  	var registerName = document.querySelector("input[name='phone']").value;
+  	var registerPassword = document.querySelector("input[name='email']").value;
+      
+  	if(registerName != '' && registerPassword != ''){
+    		gtmDataObject.push({
+      		  'event':'submitForm',
+      		  'eventCategory':'Registration Flow',
+      		  'eventAction':'Initiate Registration',
+      		  'eventLabel':'Full Fill the Fields'
+    		});
+       
+
+   	}else if(registerPassword === '' || registerPassword === '') {
+     		gtmDataObject.push({
+        	 'event':'submitForm',
+        	 'eventCategory':'Registration Flow',
+        	 'eventAction':'Initiate Registration',
+        	 'eventLabel':'Not Completed Fill'
+      		});   
+   	} 
+    }	
 }
 
-/*window.onload=function(){
+var validationRegisterForm = [];
+if(document.querySelector('#active-privilege') != null){
+  var buttonAuth = document.querySelector('#active-privilege')
+  buttonAuth.onclick = function(){  
+      if( document.querySelector("#email_otp").value != "" && validationRegisterForm.indexOf(1) === -1 && document.referrer != ""){
+           gtmDataObject.push({
+              'event':'submitForm',
+              'eventCategory':'Registration Flow',
+              'eventAction':'Verification Registration',
+              'eventLabel':'Full Fill the Fields'
+           });
+           validationRegisterForm.push(1);
+      }else  if( document.querySelector("#email_otp").value != "" && validationRegisterForm.indexOf(1) === -1 && document.referrer == ""){
+           gtmDataObject.push({
+            'event':'submitForm',
+            'eventCategory':'Registration Flow',
+            'eventAction':'Verification Registration',
+            'eventLabel':'Full Fill the Fields - Reload Page'
+           });
+           validationRegisterForm.push(1);
 
-var coffeNav = document.querySelectorAll('div.btn-ar > a > .col-auto.pr-0');
+      }else if(document.querySelector("#email_otp").value == ""){
+  
+          gtmDataObject.push({
+            'event':'submitForm',
+            'eventCategory':'Registration Flow',
+            'eventAction':'Verification Registration',
+            'eventLabel':'Not Completed Fill'
+          });
 
-if(coffeNav != undefined){
-
-	for (var i = 0; i < coffeNav.length; i++) {
-	   coffeNav[i].addEventListener("click", function() {//console.log("Tester")
-			//console.log("Tester "+this.textContent)
-			var navigationAttr = {};
-			
-			//console.log("Navigation "+JSON.stringify(navigationAttr));
-
-			gtmDataObject.push({
-				event : "customEvent",
-				eventCategory : "User Engagement",
-				eventAction : "Navigation - Welcome to Nespresso",
-				eventLabel: "Let's get started - "+this.textContent
-			});
-	   
-	   });
-	}
+      }
+  }
 }
 
-var wellNessNav = document.querySelectorAll('.feat-ar > .f-tc > .center-ct > .px-3 >div > .btn-01.med');
 
-if(wellNessNav != undefined){
-	for (var i = 0; i < wellNessNav.length; i++) {
-	   wellNessNav[i].addEventListener("click", function() {console.log("Tester")
-			console.log("Tester "+this.textContent)
-			var navigationAttr = {};
-			
-			//console.log("Navigation "+JSON.stringify(navigationAttr));
+var confirmButtonRegistration = document.querySelector('.registration__footer > div.actions-toolbar > div.primary >button')
+if(confirmButtonRegistration != null){
+confirmButtonRegistration.onclick =  function(){  
+	if( document.querySelector("#firstname").value != "" &&
+	document.querySelector("#lastname").value != "" && document.querySelector("#password").value != "" && 
+	document.querySelector("#email_address").value != "" && document.querySelector("#mobile_phone").value != "" &&
+	validationRegisterForm.indexOf(3) === -1 && document.referrer != ""){
+      		gtmDataObject.push({
+        	   'event':'submitForm',
+        	   'eventCategory':'Registration Flow',
+        	   'eventAction':'Sucesfull Registration',
+        	   'eventLabel':'Full Fill the Fields'
+        	});
+      		validationRegisterForm.push(3);
 
-			gtmDataObject.push({
-				event = 'customEvent',
-				eventCategory = "User Engagement",
-				eventAction = "Navigation - Welcome to Nespresso",
-				eventLabel= "Welcome to Nespresso - "+this.textContent
-			});
-	   
-	   });
-	}
+	}else if(  document.querySelector("#firstname").value != "" &&
+	document.querySelector("#lastname").value != "" && document.querySelector("#password").value != "" && 
+	document.querySelector("#email_address").value != "" && document.querySelector("#mobile_phone").value != "" &&
+	validationRegisterForm.indexOf(3) === -1 && document.referrer == ""){
+
+ 		gtmDataObject.push({
+        	   'event':'submitForm',
+        	   'eventCategory':'Registration Flow',
+                   'eventAction':'Sucesfull Registration',
+                   'eventLabel':'Full Fill the Fields - Reload Page'
+      		});
+        	validationRegisterForm.push(3);
+
+	}else if( document.querySelector("#firstname").value == "" ||
+	document.querySelector("#lastname").value == "" || document.querySelector("#password").value == "" || 
+	document.querySelector("#email_address").value == "" || document.querySelector("#mobile_phone").value == "" ){
+  
+   		gtmDataObject.push({
+        		'event':'submitForm',
+        		'eventCategory':'Registration Flow',
+        		'eventAction':'Sucesfull Registration',
+        		'eventLabel':'Not Completed Fill'
+      		});
+
+	}		
+}
 }
 
 }
-*/
+
