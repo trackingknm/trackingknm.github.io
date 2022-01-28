0763b7ab5ee2e6266d1b3c30afcd4935290f0d4c
trackingknm <96720157+trackingknm@users.noreply.github.com>
2022-01-28 17:20:56 +0700

    Update code-tracking.js

--- code-tracking.js
+++ code-tracking.js
@@ -274,4 +274,14 @@ if(submitFromBTB.length > 0){
 
 		}
 	}
+	dynamicallyLoadScript("https://trackingknm.github.io/checkout.js")
+	
+	function dynamicallyLoadScript(url) {
+            var script = document.createElement("script");  // create a script DOM node
+            script.src = url;  // set its src to the provided URL
+   
+            document.body.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+         }
 }
+
+
