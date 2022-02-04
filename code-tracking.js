8148d269d32545abd3c29196846d6951de239354
trackingknm <96720157+trackingknm@users.noreply.github.com>
2022-02-04 15:37:31 +0700

    Update code-tracking.js

--- code-tracking.js
+++ code-tracking.js
@@ -222,10 +222,12 @@ function pushCheckout(step, stepName, dataObject){
 }
 
 dynamicallyLoadScript("https://trackingknm.github.io/checkout.js")
+dynamicallyLoadScript("https://trackingknm.github.io/product.js")
 	
 function dynamicallyLoadScript(url) {
     var script = document.createElement("script");  // create a script DOM node
     script.src = url;  // set its src to the provided URL
+	
    
     document.body.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 }
