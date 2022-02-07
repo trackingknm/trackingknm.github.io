573e93e95ea87ffcb7f1b9ed966f7794d0620b41
trackingknm <96720157+trackingknm@users.noreply.github.com>
2022-02-07 10:33:19 +0700

    Update product.js

--- product.js
+++ product.js
@@ -1,3 +1,80 @@
+window.onload = function () {
+	var productsElement = document.querySelectorAll(".product");
+
+if(productsElement.length > 0){
+	for (var i = 0; i < productsElement.length; i++) { 
+		productsElement[i].addEventListener("click", function() {
+		    var product = [];
+			var prdCategoryElement = productsElement[0].parentElement.parentElement.querySelector(":scope >h2");
+			//var product = productsElement[i];
+			var prdIdElement = this.querySelector(":scope > .product-text > .product-price > .price-box.price-final_price");
+			var prdNameElement = this.querySelector(":scope > .product-name > h3 > a");
+			var prdPriceElement = this.querySelector(":scope > .product-text > .product-price > .price-box.price-final_price > .price-container > span");
+			var productData = {};
+					
+			var productName = "";
+					
+					
+			if(prdNameElement != null){
+				productName = prdNameElement.textContent
+			}else{
+				prdNameElement = product.querySelector(":scope > .product-text > .product-description > .product-name >h3 > a");
+				productName = prdNameElement.textContent
+			}
+						
+			if(prdCategoryElement != null){
+				prdCategory = prdCategoryElement.texContent;
+			}else{
+				prdCategory = "";	
+			}
+					
+			var productPrice = 0;
+			if(prdPriceElement == null){
+				var prdPriceElement = this.querySelector(":scope > .product-text > .product-price > input");
+				productPrice = prdPriceElement.value;
+			    //console.log("Product Price "+productPrice)
+			}else{
+				productPrice = prdPriceElement.getAttribute("data-price-amount");
+			}
+						
+			//console.log(prdCategory)
+			//console.log(productName)
+			//console.log(prdIdElement.getAttribute("data-product-id"))
+		   // console.log(productPrice)
+						
+			productData.id = prdIdElement.getAttribute("data-product-id");
+			productData.name = productName;
+			productData.price = productPrice;
+			productData.category = prdCategory;
+			productData.position = i;
+			
+			product.push(productData)
+			productClickPush(product)
+			
+		})
+	}
+}
+	
+function productClickPush(entityProduct){
+	gtmDataObject.push({
+		'event':'productClick',
+		'eventCategory':'Ecommerce',
+		'eventAction':'Product Click',
+		'eventLabel':entityProduct[0].category,
+		'ecommerce':{
+			'click': {
+				'actionField': {'list': entityProduct[0].category},      // Optional list property.
+				'products': entityProduct
+			}
+		}
+		
+	});
+
+}
+	
+}
+
+
 //product impression
 var productCtg = document.querySelectorAll(".product-category");
 var listProductIndex = [];
@@ -65,61 +142,7 @@ window.addEventListener('scroll', function() {
 	}
 })
 
-var productsElement = document.querySelectorAll(".product");
 
-if(productsElement.length > 0){
-	for (var i = 0; i < productsElement.length; i++) { 
-		productsElement[i].addEventListener("click", function() {
-		    var product = [];
-			var prdCategoryElement = productsElement[0].parentElement.parentElement.querySelector(":scope >h2");
-			//var product = productsElement[i];
-			var prdIdElement = this.querySelector(":scope > .product-text > .product-price > .price-box.price-final_price");
-			var prdNameElement = this.querySelector(":scope > .product-name > h3 > a");
-			var prdPriceElement = this.querySelector(":scope > .product-text > .product-price > .price-box.price-final_price > .price-container > span");
-			var productData = {};
-					
-			var productName = "";
-					
-					
-			if(prdNameElement != null){
-				productName = prdNameElement.textContent
-			}else{
-				prdNameElement = product.querySelector(":scope > .product-text > .product-description > .product-name >h3 > a");
-				productName = prdNameElement.textContent
-			}
-						
-			if(prdCategoryElement != null){
-				prdCategory = prdCategoryElement.texContent;
-			}else{
-				prdCategory = "";	
-			}
-					
-			var productPrice = 0;
-			if(prdPriceElement == null){
-				var prdPriceElement = this.querySelector(":scope > .product-text > .product-price > input");
-				productPrice = prdPriceElement.value;
-			    //console.log("Product Price "+productPrice)
-			}else{
-				productPrice = prdPriceElement.getAttribute("data-price-amount");
-			}
-						
-			//console.log(prdCategory)
-			//console.log(productName)
-			//console.log(prdIdElement.getAttribute("data-product-id"))
-		    console.log(productPrice)
-						
-			productData.id = prdIdElement.getAttribute("data-product-id");
-			productData.name = productName;
-			productData.price = productPrice;
-			productData.category = prdCategory;
-			productData.position = i;
-			
-			product.push(productData)
-			productClickPush(product)
-			
-		})
-	}
-}
 
 function getPositionElementByPosition(position){
 	//var element = document.querySelector(topField+topBannerSelector);
@@ -151,22 +174,7 @@ function productImpressionPush(entityProduct){
 
 }
 
-function productClickPush(entityProduct){
-	gtmDataObject.push({
-		'event':'productClick',
-		'eventCategory':'Ecommerce',
-		'eventAction':'Product Click',
-		'eventLabel':entityProduct[0].category,
-		'ecommerce':{
-			'click': {
-				'actionField': {'list': entityProduct[0].category},      // Optional list property.
-				'products': entityProduct
-			}
-		}
-		
-	});
 
-}
 
 function getNameOfLink(link){
   var lastSlash = link.lastIndexOf("/")
